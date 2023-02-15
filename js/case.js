8c9d3574f0bf45e5595ec3e827d401d3fc5624ca
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-02-16 00:19:50 +0600

    carting function working

--- js/case.js
+++ js/case.js
@@ -1,8 +1,22 @@
 
+/* 
+1.Addd event listener to the case plus button
+2.Get the value inside the case number fidld (input filed)
+3.Convert the number to an integer
+4.set the value to the case number field
+*/
 document.getElementById("btn-case-plus").addEventListener("click",function(){
 
   const caseNumberField = document.getElementById("case-number-field");
   const previousCaseNumber = parseInt(caseNumberField.value);
   const newCaseNumber = previousCaseNumber + 1;
     caseNumberField.value = newCaseNumber;
+});
+
+document.getElementById("btn-case-minus").addEventListener("click",function(){
+  const caseNumberField = document.getElementById("case-number-field");
+  const caseNumberMinus = parseInt(caseNumberField.value);
+  const newCaseNumber = caseNumberMinus - 1;
+  caseNumberField.value = newCaseNumber;  
+
 })
